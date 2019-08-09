6acf698e7aa6a61bf0a6135de404190ba4dbedcf
stupidguy176 <hoaian176@gmail.com>
2019-08-09 20:13:58 +0700

    Inittial commit

---
+++ src/android/MathCalculator.java
@@ -0,0 +1,55 @@
+package cordova-plugin-mathcalculator;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * This class echoes a string called from JavaScript.
+ */
+public class MathCalculator extends CordovaPlugin {
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("add")) {
+            this.add(args, callbackContext);
+            return true;
+        }
+        if (action.equals("substract")) {
+            this.substract(args, callbackContext);
+            return true;
+        }
+        return false;
+    }
+
+    private void add(JSONArray args, CallbackContext callback) {
+        if (args != null) {
+            try {
+                int p1 = Integer.parseInt(args.getJSONObject(0).getString("param1"));
+                int p2 = Integer.parseInt(args.getJSONObject(0).getString("param2"));
+                callback.success("result" + (p1 + p2));
+            } catch (Exception ex) {
+                callback.error("Somethings went wrong " + ex);
+            }
+        } else {
+            callback.error("Please dont pass null value");
+        }
+    }
+
+    private void substract(JSONArray args, CallbackContext callback) {
+        if (args != null) {
+            try {
+                int p1 = Integer.parseInt(args.getJSONObject(0).getString("param1"));
+                int p2 = Integer.parseInt(args.getJSONObject(0).getString("param2"));
+                callback.success("result" + (p1 - p2));
+            } catch (Exception ex) {
+                callback.error("Somethings went wrong " + ex);
+            }
+        } else {
+            callback.error("Please dont pass null value");
+        }
+    }
+}
